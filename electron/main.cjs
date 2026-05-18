9156bcbd851e0c254999b1456b8dfa5b6e5f6c99
zhangxiao <zhangxiao@foherb.com>
2026-05-18 16:51:08 +0800

    feat: 打包配置完成

--- electron/main.cjs
+++ electron/main.cjs
@@ -80,7 +80,7 @@ function createWindow() {
     mainWindow.webContents.openDevTools()
   } else {
     mainWindow.loadFile(path.join(__dirname, '../dist/index.html'))
-    mainWindow.webContents.openDevTools()
+    // mainWindow.webContents.openDevTools()
   }
 
   mainWindow.on('closed', () => {
